d3992006085e2d92c9592b1e8f18778b039851b9
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2021-10-11 22:38:44 +0200

    security

--- jsxgraph.js
+++ jsxgraph.js
@@ -1,8 +1,8 @@
 var H5P = H5P || {};
 
 H5P.JSXGraph = (function ($) {
-//   var $ = H5P.jQuery;
-//   this.$ = $(this);
+  //   var $ = H5P.jQuery;
+  //   this.$ = $(this);
 
   var depurify = function (str) {
     return str.replace(/&#039;/g, "'").
@@ -17,7 +17,9 @@ H5P.JSXGraph = (function ($) {
    */
   function C(options, id) {
     // Extend defaults with provided options
+
     this.options = $.extend(true, {}, {
+      resizeSupported: true,
       commentPre: 'BEFORE',
       commentPost: 'AFTER',
       advanced: {
@@ -30,6 +32,7 @@ H5P.JSXGraph = (function ($) {
       },
       code: ''
     }, options);
+
     // Keep provided id.
     this.id = id;
   };
@@ -42,9 +45,10 @@ H5P.JSXGraph = (function ($) {
    */
   C.prototype.attach = function ($container) {
     var outerPre, outerPost, divId,
-      composeCSS = function (options) {
+      options = this.options,
+      composeCSSForJXG = function (options) {
         var txt = '',
-            res, w, h;
+          res, w, h;
         if (options.advanced.showFixedSize) {
           txt += 'width:' + options.advanced.width + '; ';
           txt += 'height:' + options.advanced.height + '; ';
@@ -59,15 +63,15 @@ H5P.JSXGraph = (function ($) {
           res = options.advanced.aspectRatio.match(/\w*(\d+)\w*\/\w*(\d+)\w*/);
           w = parseFloat(res[1]);
           h = parseFloat(res[2]);
-          txt += 'height:0; padding-bottom:' + (100*h/w) + '%;'
+          txt += 'height:0; padding-bottom:' + (100 * h / w) + '%;'
         }
 
         return txt;
       };
 
-    if (this.options.divId !== undefined && this.options.divId !== '') {
+    if (options.divId !== undefined && options.divId !== '') {
       // Take the supplied div Id.
-      divId = this.options.divId;
+      divId = options.divId;
     }
     else {
       // Choose random div Id.
@@ -75,63 +79,78 @@ H5P.JSXGraph = (function ($) {
     }
 
     // Set class on container to identify it as a JSXGraph construction.
-    if (!this.options.advanced.showFixedSize) {
-        // Use the old aspect-ratio hack
-        outerPre = '<div style="max-width:' + this.options.advanced.maxWidth + ';';
-        outerPre += 'margin: 0 auto;';
-        outerPre += '">'
-        outerPost = '</div>';
+    if (!options.advanced.showFixedSize) {
+      // Use the old aspect-ratio hack
+      outerPre = '<div style="max-width:' + options.advanced.maxWidth + ';';
+      outerPre += 'margin: 0 auto;';
+      outerPre += '">'
+      outerPost = '</div>';
     }
     else {
-        outerPre = '';
-        outerPost = '';
+      outerPre = '';
+      outerPost = '';
     }
     $container.addClass("h5p-jsxgraph");
 
-    var txt = '<div id="pre" class="h5p-jsxgraph-comment-post">' + this.options.commentPre + '</div>' +
-        // '<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>' +
-        // '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js"></script>' +
-        // '<script src="https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraphcore.js" type="text/javascript"></script>' +
+    var headTxt = '<head><meta http-equiv="Content-Security-Policy" content="navigate-to \'none\'">';
+
+    var path = "/sites/default/files/h5p/development/H5P.JSXGraph/"
+    headTxt +=
+      // '<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>' +
+      // '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js"></script>' +
+      // '<script type="text/javascript" src="/sites/default/files/h5p/libraries/H5P.MathDisplay-1.0/scripts/mathdisplay.js?r0sz7p"></script>' +
+      '<script src="' + path + 'jsxgraphcore.js" type="text/javascript"></script>' +
+      '<link rel="stylesheet" href="' + path + 'h5p-jsxgraph.css" type="text/css">' +
+      '<link rel="stylesheet" href="' + path + 'jsxgraph.css" type="text/css">';
+
+    var secureJS = '<script type="text/javascript">' +
+      'XMLHttpRequest = {};' +
+      'WebSocket = {};' +
+      '</script>';
+
+    headTxt += secureJS;
+    headTxt += '</head>';
+
+    var bodyTxt = '<body>' +
+      '<div id="pre" class="h5p-jsxgraph-comment-post">' + options.commentPre + '</div>' +
       outerPre +
       '<div id="' + divId + '" class="jxgbox" ' +
-        'style="' + composeCSS(this.options) + '"' +
+      'style="' + composeCSSForJXG(options) + '"' +
       '></div>' +
       outerPost +
+      '' +
       '<script type="text/javascript">' +
-        'XMLHttpRequest = {};' +
-        'WebSocket = {};' +
-        //'H5PIntegration = {};' +
-        //'HTMLFormElement.prototype.onsubmit = function (){};' +
-        depurify(this.options.code).replace(/BOARDID/g, "'" + divId + "'") +
+      depurify(options.code).replace(/BOARDID/g, "'" + divId + "'") +
       '</script>' +
       '<div id="post" class="h5p-jsxgraph-comment-post">'
-        + this.options.commentPost +
+      + options.commentPost +
       '</div>';
 
-    $container.append(txt);
+    bodyTxt += '</body>';
 
-    // $iframe = $('<iframe/>', {
-    //     src: 'about:blank',
-    //     scrolling: 'no',
-    //     frameBorder: 0,
-    //     sandbox: 'allow-same-origin allow-scripts',
-    //     'class': 'h5p-iframe-content h5p-iframe-wrapper',
-    //     css: {width:'500px', height: '500px'}
-    //   });
+    //$container.append(headTxt + bodyTxt);
 
-    //   console.log(composeCSS(this.options));
-    // $container.html('');
-    // $container.append($iframe);
+    $iframe = $('<iframe id="xxx" src="about:blank" scrolling="no" frameborder="0" sandbox="allow-same-origin allow-scripts" class="h5p-iframe-content h5p-iframe-wrapper" />');
 
-    // var x = document.getElementsByTagName("iframe")[0];
-    // x.contentWindow.document.open('text/html', 'replace');
-    // x.contentWindow.document.write(txt);
-    // x.contentWindow.document.close();
+    $container.html('');
+    $container.append($iframe);
+    var doc = $iframe.contents()[0];
 
-    // x.sandbox.toggle('allow-same-origin');
-    //x.sandbox.toggle('allow-scripts');
-    //x.contentWindow.location.href = "about:blank";
-    //console.log(x.sandbox);
+    doc.open('text/html', 'replace');
+    doc.write(headTxt);
+    doc.write(bodyTxt);
+    doc.close();
+
+    $iframe.css({ width: '100%', height: this.options.advanced.height});
+    // var node = document.getElementById('xxx'); //contentWindow.document.getElementsByTagName('body').item(0);
+    // var h = node.contentWindow.document.body.scrollHeight;
+    // console.log(node.contentWindow.document.body);
+
+    //console.log(H5P.getLibraryConfig('H5P.JSXGraph'));
+    //console.log(H5P.getContent());
+    //console.log(H5P.getContentForInstance());
+    // console.log(H5P);
+
   };
 
   return C;
